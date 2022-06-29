4cd36b954833055c0e76ae1a8a87c86fba51547e
Andres Felipe Vera Castañeda <ciberandresoficial@gmail.com>
2022-06-29 09:17:53 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CodigoDeBarras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PBCodBarra = new System.Windows.Forms.PictureBox();
+            this.TxtCodBarra = new System.Windows.Forms.TextBox();
+            this.BtnGenerar = new System.Windows.Forms.Button();
+            this.TxtBuscarCodBarra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnInsertar = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PBCodBarra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PBCodBarra
+            // 
+            this.PBCodBarra.Location = new System.Drawing.Point(12, 12);
+            this.PBCodBarra.Name = "PBCodBarra";
+            this.PBCodBarra.Size = new System.Drawing.Size(334, 118);
+            this.PBCodBarra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBCodBarra.TabIndex = 0;
+            this.PBCodBarra.TabStop = false;
+            // 
+            // TxtCodBarra
+            // 
+            this.TxtCodBarra.Location = new System.Drawing.Point(17, 136);
+            this.TxtCodBarra.Name = "TxtCodBarra";
+            this.TxtCodBarra.Size = new System.Drawing.Size(161, 20);
+            this.TxtCodBarra.TabIndex = 1;
+            this.TxtCodBarra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // BtnGenerar
+            // 
+            this.BtnGenerar.Location = new System.Drawing.Point(190, 134);
+            this.BtnGenerar.Name = "BtnGenerar";
+            this.BtnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerar.TabIndex = 2;
+            this.BtnGenerar.Text = "Generar";
+            this.BtnGenerar.UseVisualStyleBackColor = true;
+            this.BtnGenerar.Click += new System.EventHandler(this.BtnGenerar_Click);
+            // 
+            // TxtBuscarCodBarra
+            // 
+            this.TxtBuscarCodBarra.Location = new System.Drawing.Point(55, 162);
+            this.TxtBuscarCodBarra.Name = "TxtBuscarCodBarra";
+            this.TxtBuscarCodBarra.Size = new System.Drawing.Size(161, 20);
+            this.TxtBuscarCodBarra.TabIndex = 3;
+            this.TxtBuscarCodBarra.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtBuscarCodBarra_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Buscar";
+            // 
+            // BtnInsertar
+            // 
+            this.BtnInsertar.Location = new System.Drawing.Point(271, 134);
+            this.BtnInsertar.Name = "BtnInsertar";
+            this.BtnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.BtnInsertar.TabIndex = 7;
+            this.BtnInsertar.Text = "Insertar";
+            this.BtnInsertar.UseVisualStyleBackColor = true;
+            this.BtnInsertar.Click += new System.EventHandler(this.BtnInsertar_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(222, 160);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.BtnLimpiar.TabIndex = 8;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 189);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnInsertar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtBuscarCodBarra);
+            this.Controls.Add(this.BtnGenerar);
+            this.Controls.Add(this.TxtCodBarra);
+            this.Controls.Add(this.PBCodBarra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PBCodBarra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PBCodBarra;
+        private System.Windows.Forms.TextBox TxtCodBarra;
+        private System.Windows.Forms.Button BtnGenerar;
+        private System.Windows.Forms.TextBox TxtBuscarCodBarra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnInsertar;
+        private System.Windows.Forms.Button BtnLimpiar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//Libreria Codigo de Barras
+using ZXing;
+
+namespace CodigoDeBarras
+{
+    public partial class Form1 : Form
+    {
+        public static string cadena = "Server=CARAPRCTIFSD163\\SQLEXPRESS;Database=Datos;User Id=ADSI2278769;Password=12345;";
+        public static SqlConnection conexion = new SqlConnection(cadena);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                MessageBox.Show("Hola");
+            }
+        }
+
+        private void BtnGenerar_Click(object sender, EventArgs e)
+        {
+            string ruta = Application.StartupPath + "\\CodBarras.bmp";
+                // instancia de la funcion de la libreria Zxing
+                var barcodeWriter = new BarcodeWriter();
+
+            // set the barcode format
+            barcodeWriter.Format = BarcodeFormat.CODE_128;
+
+            // write text and generate a 2-D barcode as a bitmap
+            barcodeWriter
+                .Write(TxtCodBarra.Text)
+                .Save(Application.StartupPath + "\\CodBarras.bmp");
+
+                //muestro en el picture box
+                PBCodBarra.Image=Image.FromFile(ruta);
+        }
+
+        private void BtnInsertar_Click(object sender, EventArgs e)
+        {
+            SqlCommand comando = new SqlCommand("PA_INSERTARCODBAR", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", TxtCodBarra.Text);
+            string ruta = Application.StartupPath + "\\CodBarras.bmp";
+            Image ImagenCodBar = Image.FromFile(ruta);
+            //Convertir a uun arreglo de byte
+            MemoryStream ms;
+            ms = new MemoryStream();
+            ImagenCodBar.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] photo_aray = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(photo_aray, 0, photo_aray.Length);
+            comando.Parameters.AddWithValue("@IMAGEN", photo_aray);
+            //ejecutar
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+            MessageBox.Show("Imagen de codigo de barras insertada");
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            TxtCodBarra.Clear();
+        }
+
+        private void TxtBuscarCodBarra_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SqlDataAdapter comando = new SqlDataAdapter("PA_TRAERIMAGENCODBAR", conexion);
+            comando.SelectCommand.CommandType = CommandType.StoredProcedure;
+            comando.SelectCommand.Parameters.AddWithValue("@id", TxtBuscarCodBarra.Text);
+            DataTable tabla = new DataTable();
+            comando.Fill(tabla);
+            //capturo el arreglo de bytes
+            byte[] photo_aray = (byte[])tabla.Rows[0]["imagen"];
+            ///convierte los bytes a una imagen
+            MemoryStream ms=new MemoryStream(photo_aray);
+            Bitmap bmp = null;
+            bmp= new Bitmap(ms);
+            //Muestro la imagen
+            PBCodBarra.Image=bmp;
+            MessageBox.Show("Imagen de codigo de barras insertada");
+        }
+    }
+}
